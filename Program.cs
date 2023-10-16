1a282ca16986fd88e2e83ecf31ac1f09089bc235
motor <motor@LAPTOP-KVUJ9B6A>
2023-10-17 02:24:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ship
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Ship(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+class GameField
+{
+    private int[,] mas1;
+    private int[,] mas2;
+    private List<string> movesHistory;
+    private int hitCount;
+
+    public GameField()
+    {
+        mas1 = new int[10, 10];
+        mas2 = new int[10, 10];
+        movesHistory = new List<string>();
+        hitCount = 0;
+    }
+
+    public void Initialize()
+    {
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if ((i % 2) == 0)
+                {
+                    mas1[i, j] = rand.Next(0, 2);
+                }
+                else
+                {
+                    mas1[i, j] = 0;
+                }
+            }
+        }
+    }
+
+    public void DrawField()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write("[ ]");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void PlaceShips()
+    {
+        int x, y;
+
+        for (int t = 0; t < 50; t++)
+        {
+            Console.WriteLine("введите координаты корабля через Enter:");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            y--;
+            x--;
+            Console.Clear();
+
+            if (mas1[x, y] == 1)
+            {
+                mas2[x, y] = 1;
+                hitCount++;
+                movesHistory.Add($"ход {t + 1}: игрок попал в корабль по координатам [{x + 1}, {y + 1}]");
+            }
+            else
+            {
+                mas2[x, y] = 2;
+                movesHistory.Add($"ход {t + 1}: игрок промахнулся по координатам [{x + 1}, {y + 1}]");
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (mas2[i, j] == 0)
+                    {
+                        Console.Write("[ ]");
+                    }
+                    if (mas2[i, j] == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("[x]");
+                        Console.ResetColor();
+                    }
+                    if (mas2[i, j] == 2)
+                    {
+                        Console.Write("[o]");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("количество попаданий: " + hitCount);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nистория ходов игрока:");
+            foreach (var move in movesHistory)
+            {
+                Console.WriteLine(move);
+            }
+            Console.ResetColor();
+        }
+        Console.Read();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GameField gameField = new GameField();
+        gameField.Initialize();
+        gameField.DrawField();
+        gameField.PlaceShips();
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("sea_battle_3.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sea_battle_3.0")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("df9450cd-1d9d-4016-9200-08877d1d7748")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
